b0e961aedc2e49e1a8c6b2cd5c63fca02b7c9756
thiago <tgandr@gmail.com>
2024-05-29 22:01:28 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Prof. Thiago Andrade da Silva
+          M.Sc. in Fishing Engineering
+          Instituto Federal de Educação, Ciência e Tecnologia do Ceará
+        </p>
+        <a
+          className="App-link"
+          href="https://aquiculturanoceara.blogspot.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Blog Aquicultura no Ceará
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
