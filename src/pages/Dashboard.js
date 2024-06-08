25a4d14239ce4a2acfd6c81da66a65919609cf3b
thiago <tgandr@gmail.com>
2024-06-08 15:15:27 -0300

    atualização da dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import aquaDataIcon from '../assets/images/aqua-data-icon-512.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShrimp, faBox, faMoneyBill, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faShrimp, faBox, faDollarSign, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import '../styles/Dashboard.css';
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -16,27 +18,56 @@ const Dashboard = () => {
     navigate('/login');
   };
 
+  const formData = JSON.parse(localStorage.getItem('formData'));
+
   return (
     <div className="dashboard-container">
-      <h2>Início</h2>
+      <div className="identify-data">
+        <h2>Início</h2>
+        <h3>Fazenda {formData.nomeFazenda}</h3>
+      </div>
       <div className="btn-container">
-        <button onClick={() => navigate('/viveiros')}>
-          <FontAwesomeIcon icon={faShrimp} /> Produção
+        <button className="dashboard-button" onClick={() => navigate('/viveiros')}>
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon={faShrimp} className="icon" />
+          </div>
+          <div className="text-wrapper">
+            <span>Produção</span>
+          </div>
         </button>
-        <button onClick={() => navigate('/estoque')}>
-          <FontAwesomeIcon icon={faBox} /> Controle de Estoque
+        <button className="dashboard-button" onClick={() => navigate('/estoque')}>
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon={faBox} className="icon" />
+          </div>
+          <div className="text-wrapper">
+            <span>Controle de Estoque</span>
+          </div>
         </button>
-        <button onClick={() => navigate('/financeiro')}>
-          <FontAwesomeIcon icon={faMoneyBill} /> Financeiro
+        <button className="dashboard-button" onClick={() => navigate('/financeiro')}>
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon={faDollarSign} className="icon" />
+          </div>
+          <div className="text-wrapper">
+            <span>Financeiro</span>
+          </div>
         </button>
-        <button onClick={handleLogout}>
-          <FontAwesomeIcon icon={faSignOutAlt} /> Sair da Conta
+        <button className="dashboard-button" onClick={handleLogout}>
+          <div className="icon-wrapper">
+            <FontAwesomeIcon icon={faSignOutAlt} className="icon" />
+          </div>
+          <div className="text-wrapper">
+            <span>Sair da Conta</span>
+          </div>
         </button>
-      </ div>
+      </div>
       <div className="icon-container">
-        <img src={aquaDataIcon} alt="Aqua Data Icon" style={{ width: '100px', height: '100px' }} />
+        <img src={aquaDataIcon} alt="Aqua Data Icon" />
+      </div>
+      <div className="releases">
+        <FontAwesomeIcon icon={faInstagram} /><span> data.aqua</span>
       </div>
     </div>
+
   );
 };
 
