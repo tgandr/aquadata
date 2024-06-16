2e573f00eab127414da12a29985b5c61581bcd75
thiago <tgandr@gmail.com>
2024-06-16 01:24:48 -0300

    ajustes no lançamento de marcas de ração

--- src/pages/RationPurchasesPopup.js
+++ src/pages/RationPurchasesPopup.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 
-const RationPurchasesPopup = ({setShowRationPurchasesPopup}) => {
+const RationPurchasesPopup = ({ setShowRationPurchasesPopup }) => {
     const [addNewBrandPopup, setAddNewBrandPopup] = useState(false);
     const [showSavedMessage, setShowSavedMessage] = useState(false);
-    const [ratiosBrands, setRatiosBrands] = useState([]);
+    const [rationsBrands, setRationsBrands] = useState([]);
     const [addNewBrand, setAddNewBrand] = useState('');
     const [formRation, setFormRation] = useState({
         purchaseDate: '',
@@ -14,50 +14,72 @@ const RationPurchasesPopup = ({setShowRationPurchasesPopup}) => {
         prizePerBag: '',
         feedType: ''
     });
+    const [addBrand, setAddBrand] = useState('');
+    const [brands, setBrands] = useState(["Aquavita", "Biotê", "Guabi", "Integral", "Poli Nutri", "Presence", "Samaria", "Total"]);
+    const [showFieldNewBrand, setShowFieldNewBrand] = useState(false);
 
     useEffect(() => {
         const storedStockData = JSON.parse(localStorage.getItem('stockData')) || {};
         if ('brandRatioList' in storedStockData) {
-            setRatiosBrands(storedStockData.brandRatioList);
+            setRationsBrands(storedStockData.brandRatioList);
         }
     }, []);
 
     useEffect(() => {
-    if (formRation.brand === 'custom') {
-        setAddNewBrandPopup(true);
-    } else {
-        setAddNewBrandPopup(false);
-    }}, [formRation]);
+        if (formRation.brand === 'custom') {
+            setAddNewBrandPopup(true);
+        } else {
+            setAddNewBrandPopup(false);
+        }
+    }, [formRation.brand]);
+
+    useEffect(() => {
+        if (addBrand === 'custom') {
+            setShowFieldNewBrand(true);
+        } else {
+            setShowFieldNewBrand(false);
+        }
+    }, [addBrand]);
 
     const handleChange = (e) => {
-        setFormRation({... formRation, [e.target.name]: e.target.value})
+        setFormRation({ ...formRation, [e.target.name]: e.target.value });
         console.log(e.target.value);
     };
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
         let financial = JSON.parse(localStorage.getItem('financial')) || {};
         let formRationCheckOut = formRation;
-            if (financial) {
-                if ('feedPurchase' in financial) {
-                    formRationCheckOut = {...formRationCheckOut, purchaseId: {
+        if (financial) {
+            if ('feedPurchase' in financial) {
+                formRationCheckOut = {
+                    ...formRationCheckOut,
+                    purchaseId: {
                         purchase: 'ration',
-                        id: financial.feedPurchase.length}
+                        id: financial.feedPurchase.length
                     }
-                    financial.feedPurchase.push(formRationCheckOut);
-                } else {
-                    formRationCheckOut = {...formRationCheckOut, purchaseId: {
+                }
+                financial.feedPurchase.push(formRationCheckOut);
+            } else {
+                formRationCheckOut = {
+                    ...formRationCheckOut,
+                    purchaseId: {
                         purchase: 'ration',
                         id: 1
-                    }}
-                    financial = {...financial, feedPurchase: [formRationCheckOut]}
+                    }
                 }
-            } else {
-                formRationCheckOut = {...formRationCheckOut, purchaseId: {
+                financial = { ...financial, feedPurchase: [formRationCheckOut] }
+            }
+        } else {
+            formRationCheckOut = {
+                ...formRationCheckOut,
+                purchaseId: {
                     purchase: 'ration',
                     id: 1
-                }}
-                financial = {feedPurchase: [formRationCheckOut]}
+                }
             }
+            financial = { feedPurchase: [formRationCheckOut] }
+        }
         localStorage.setItem('financial', JSON.stringify(financial));
         setFormRation({
             purchaseDate: '',
@@ -75,16 +97,16 @@ const RationPurchasesPopup = ({setShowRationPurchasesPopup}) => {
         let stockData = JSON.parse(localStorage.getItem('stockData')) || {};
         if (stockData) {
             if ('brandRatioList' in stockData) {
-            stockData.brandRatioList.push(addNewBrand);
+                stockData.brandRatioList.push(addNewBrand);
             } else {
-            stockData = { ...stockData, brandRatioList: [addNewBrand] };
-        }
+                stockData = { ...stockData, brandRatioList: [addNewBrand] };
+            }
         } else {
             stockData = { brandRatioList: [addNewBrand] };
         }
         localStorage.setItem('stockData', JSON.stringify(stockData));
-        setFormRation({...formRation, brand: addNewBrand})
-        setRatiosBrands(stockData.brandRatioList)
+        setFormRation({ ...formRation, brand: addNewBrand });
+        setRationsBrands(stockData.brandRatioList);
         setAddNewBrand('');
         setShowSavedMessage(true);
         setTimeout(() => setShowSavedMessage(false), 2000);
@@ -93,113 +115,127 @@ const RationPurchasesPopup = ({setShowRationPurchasesPopup}) => {
     return (
         <div>
             <div className="popup">
-            <div className="popup-inner">
-            <h3>Adicionar Ração</h3>
-            <form onSubmit={ (e) => handleSubmit(e) }>
-                <label>
-                    Data da Compra:
-                    <input
-                        type="date"
-                        name="purchaseDate"
-                        value={formRation.purchaseDate}
-                        onChange={(e) => handleChange(e)}
-                        required />
-                </label>
-                <label>
-                    Marca da Ração:
-                    <select 
-                        name="brand"
-                        value={formRation.brand}
-                        onChange={(e) => handleChange(e)}
-                        required>
-                            <option value="">Indique o fabricante de ração</option>
-                            {ratiosBrands.map((brand, i) => (
+                <div className="popup-inner">
+                    <h3>Adicionar Ração</h3>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            Data da Compra:
+                            <input
+                                type="date"
+                                name="purchaseDate"
+                                value={formRation.purchaseDate}
+                                onChange={handleChange}
+                                required />
+                        </label>
+                        <label>
+                            Marca da Ração:
+                            <select
+                                name="brand"
+                                value={formRation.brand}
+                                onChange={handleChange}
+                                required>
+                                <option value="">Indique o fabricante de ração</option>
+                                {rationsBrands.map((brand, i) => (
+                                    <option key={i} value={brand}>{brand}</option>
+                                ))}
+                                <option value="custom">Adicionar fabricante</option>
+                            </select>
+                        </label>
+                        <label>
+                            Tipo de Ração:
+                            <select
+                                name="feedType"
+                                value={formRation.feedType}
+                                onChange={handleChange}
+                                required>
+                                <option value="">Selecione</option>
+                                <option value="Inicial">Inicial</option>
+                                <option value="1.0 mm">1.0 mm</option>
+                                <option value="2.0 mm">2.0 mm</option>
+                                <option value="3.0 mm">3.0 mm</option>
+                            </select>
+                        </label>
+                        <label>
+                            Quantidade de Sacos:
+                            <input
+                                type="number"
+                                name="bagsQuantity"
+                                value={formRation.bagsQuantity}
+                                onChange={handleChange}
+                                required />
+                        </label>
+                        <label>
+                            Tamanho do Saco:
+                            <select
+                                name="bagSize"
+                                value={formRation.bagSize}
+                                onChange={handleChange}
+                                required>
+                                <option value="">Selecione</option>
+                                <option value="10 kg">10 kg</option>
+                                <option value="15 kg">15 kg</option>
+                                <option value="20 kg">20 kg</option>
+                                <option value="25 kg">25 kg</option>
+                                <option value="30 kg">30 kg</option>
+                            </select>
+                        </label>
+                        <label>
+                            Data de Validade:
+                            <input
+                                type="date"
+                                name="validityDate"
+                                value={formRation.validityDate}
+                                onChange={handleChange}
+                                required />
+                        </label>
+                        <label>
+                            Preço por Saco:
+                            <input
+                                type="number"
+                                name="prizePerBag"
+                                value={formRation.prizePerBag}
+                                onChange={handleChange}
+                                required />
+                        </label>
+                        <button type="submit">Salvar</button>
+                        <button type="button" onClick={() => setShowRationPurchasesPopup(false)}>Cancelar</button>
+                    </form>
+                </div>
+            </div>
+
+            {addNewBrandPopup && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <label>Cadastrar novo fabricante:</label>
+                        <select
+                            name="addBrand"
+                            value={addBrand}
+                            onChange={(e) => setAddBrand(e.target.value)}>
+                            <option value="">Selecione um fabricante</option>
+                            {brands.map((brand, i) => (
                                 <option key={i} value={brand}>{brand}</option>
                             ))}
                             <option value="custom">Adicionar fabricante</option>
-                    </select>
-                </label>
-                <label>
-                    Tipo de Ração:
-                    <select
-                        name="feedType"
-                        value={formRation.feedType}
-                        onChange={(e) => handleChange(e)}
-                        required>
-                            <option value="">Selecione</option>
-                            <option value="Inicial">Inicial</option>
-                            <option value="1.0 mm">1.0 mm</option>
-                            <option value="2.0 mm">2.0 mm</option>
-                            <option value="3.0 mm">3.0 mm</option>
-                    </select>
-                </label>
-                <label>
-                    Quantidade de Sacos:
-                    <input 
-                        type="number"
-                        name="bagsQuantity"
-                        value={formRation.bagsQuantity}
-                        onChange={(e) => handleChange(e)}
-                        required />
-                </label>
-                <label>
-                    Tamanho do Saco:
-                    <select
-                    name="bagSize"
-                    value={formRation.bagSize}
-                    onChange={(e) => handleChange(e)}
-                    required>
-                        <option value="">Selecione</option>
-                        <option value="10 kg">10 kg</option>
-                        <option value="15 kg">15 kg</option>
-                        <option value="20 kg">20 kg</option>
-                        <option value="25 kg">25 kg</option>
-                        <option value="30 kg">30 kg</option>
-                    </select>
-                </label>
-                <label>
-                    Data de Validade:
-                    <input 
-                    type="date"
-                    name="validityDate"
-                    value={formRation.validityDate}
-                    onChange={(e) => handleChange(e)}
-                    required />
-                </label>
-                <label>
-                    Preço por Saco:
-                    <input 
-                    type="number"
-                    name="prizePerBag"
-                    value={formRation.prizePerBag}
-                    onChange={(e) => handleChange(e)}
-                    required />
-                </label>
-                <button type="submit">Salvar</button>
-                <button type="button" onClick={() => setShowRationPurchasesPopup(false)}>Cancelar</button>
-            </form>
-            </div>
-        </div>
-        
-        {addNewBrandPopup && (
-            <div className="popup">
-                <div className="popup-content">
-                <label>Cadastrar novo fabricante:</label>
-                    <input
-                    type="text"
-                    value={addNewBrand}
-                    onChange={(e) => setAddNewBrand(e.target.value)}
-                    />
-                    <button onClick={() => saveNewBrand()}>Confirmar</button>
-                    <button onClick={() => setAddNewBrandPopup(false)}>Voltar</button>
+                        </select>
+                        {showFieldNewBrand && (
+                            <div>
+                                <label>Adicione novo fabricante</label>
+                                <input
+                                    type="text"
+                                    value={addNewBrand}
+                                    onChange={(e) => setAddNewBrand(e.target.value)}
+                                />
+                            </div>
+                        )}
+                        <button onClick={saveNewBrand}>Confirmar</button>
+                        <button onClick={() => setAddNewBrandPopup(false)}>Voltar</button>
+                    </div>
                 </div>
-            </div>
-        )}
-        
-        {showSavedMessage && <div className="saved-message">Salvo!</div>}
+            )}
 
-    </div>
-    )
+            {showSavedMessage && <div className="saved-message">Salvo!</div>}
+        </div>
+    );
 }
 
 export default RationPurchasesPopup;
