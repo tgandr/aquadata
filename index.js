f4a32e02b83eb06b8b88d263758e4cbc85bb0f7b
thiago <tgandr@gmail.com>
2025-01-15 15:45:44 -0300

    ajuste de cors para aceitar conexão de localhst:3000 e tgandr.github.io/login

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./src/config/db');
-// const fs = require('fs');
-// const https = require('https');  // Módulo para HTTPS
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,9 +10,7 @@ const cors = require('cors');
 
 // Middleware para CORS
 app.use(cors({
-    origin: ['http://localhost:5000', 'https://tgandr.github.io'], // Permitir a origem do GitHub Pages
-    // origin: ['http://localhost:3000', 'http://ec2-3-208-8-220.compute-1.amazonaws.com'], // Permitir a origem do GitHub Pages
-
+    origin: ['http://localhost:3000', 'https://tgandr.github.io/login'], 
     credentials: true,
 }));
 
@@ -35,33 +32,6 @@ app.post('/data', (req, res) => {
     res.send(`You sent: ${JSON.stringify(data)}`);
 });
 
-// Iniciar o servidor
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-
 app.listen(5000, () => {
     console.log('Servidor HTTP rodando na porta 5000');
 });
-
-// Caminhos para os certificados SSL gerados
-// const options = {
-//     key: fs.readFileSync('/etc/ssl/private/selfsigned.key'),
-//     cert: fs.readFileSync('/etc/ssl/certs/selfsigned.crt'),
-// };
-
-// Iniciar o servidor HTTPS
-// https.createServer(options, app).listen(443, () => {
-//     console.log(`Server is running on https://localhost:${port}`);
-// });
-
-// https.createServer(options, app).listen(5000, () => {
-//     console.log('Server is running on https://localhost:5000');
-// });
-
-// Redirecionar todo o tráfego HTTP para HTTPS
-// const http = require('http');
-// http.createServer((req, res) => {
-//     res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-//     res.end();
-// }).listen(80);  // Escutando na porta HTTP
